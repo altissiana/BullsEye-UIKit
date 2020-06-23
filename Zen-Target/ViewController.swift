dd884ab91a2c3e624801d278b34db3ba16c3a5c7
Tissiana Alves <altissiana@gmail.com>
2020-06-23 15:45:43 -0600

    added pop-up alert

--- Zen-Target/ViewController.swift
+++ Zen-Target/ViewController.swift
@@ -14,6 +14,13 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    @IBAction func showAlert() {
+        let alert = UIAlertController(title: "Hello world", message: "This is my first alert", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Awesome", style: .default, handler: nil)
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
 
 
 }
