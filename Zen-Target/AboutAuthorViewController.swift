3c7e199942d6ef77986f687674b722e39a5c1e1b
Tissiana Alves <altissiana@gmail.com>
2020-06-25 17:42:05 -0600

    added about the authro view controller and segue

---
+++ Zen-Target/AboutAuthorViewController.swift
@@ -0,0 +1,21 @@
+//
+//  AboutAuthorViewController.swift
+//  Zen-Target
+//
+//  Created by Tissiana Alves on 6/25/20.
+//  Copyright © 2020 Tissiana Alves. All rights reserved.
+//
+
+import UIKit
+
+class AboutAuthorViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func close() {
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
