b305467f6b79b3be3a4316a9c5e6f8a1813ab85e
Tissiana Alves <altissiana@gmail.com>
2020-06-25 17:36:19 -0600

    connected the view controller to about class and added close button functionality

--- Zen-Target/AboutViewController.swift
+++ Zen-Target/AboutViewController.swift
@@ -12,7 +12,9 @@ class AboutViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        
+    }
+    
+    @IBAction func close() {
+        dismiss(animated: true, completion: nil)
     }
 }
